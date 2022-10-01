7fb52f369564aa7aad25c6f9953534207d29ec29
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-01 16:41:00 +0500

    Update BasicVariablesInitialization.java

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,9 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first=1,second=10,third=1000;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
